2ddf08d44ce66e2bf43c0b6e6b9b89ce9b976d94
SVTroya <my.svtroy@gmail.com>
2023-11-02 12:19:28 +0200

    Task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,17 +1,35 @@
-import Notiflix from 'notiflix';
+import Notiflix from 'notiflix'
 
-const form = document.querySelector('form');
+const form = document.querySelector('form')
 
-form.onsubmit = () => {
-  const { delay, step, amount } = form.elements;
-  for (let i = 0; i < amount; i++) createPromise();
-};
+form.onsubmit = (ev) => {
+  ev.preventDefault()
+  const { delay, step, amount } = ev.target.elements
+  let delayStep = Number(step.value)
+  for (let position = 0, promiseDelay = Number(delay.value); position < amount.value; position++, promiseDelay += delayStep) {
+    createPromise(position, promiseDelay)
+  }
+}
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const shouldResolve = Math.random() > 0.3
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay)
+  })
+
+  promise.then(onSuccess).catch(onReject)
+}
+
+function onSuccess({ position, delay }) {
+  setTimeout(Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`), delay)
+}
+
+function onReject({ position, delay }) {
+  setTimeout(Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`), delay)
 }
